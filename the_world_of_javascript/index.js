950fdbb9c3891e97b077bb2fcb0bbea9f7bb770e
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-03-26 20:48:04 -0400

    feat(twovjs): let users to remove todo items

--- the_world_of_javascript/index.js
+++ the_world_of_javascript/index.js
@@ -12,11 +12,19 @@ addTodoButtonElem.addEventListener("click", addTodoItem);
 
 // Define event actions.
 
+function addTodoItem(event) {
   const newTodoText = addTodoTextElem.value.trim();
   if (newTodoText.length === 0) {
     return;
   }
   const newTodoItemElem = document.createElement("li");
   newTodoItemElem.textContent = newTodoText;
+  newTodoItemElem.addEventListener("click", removeTodoItem);;
   todoListElem.appendChild(newTodoItemElem);
+}
+
+function removeTodoItem(event) {
+  const clickedTodoItem = event.target;
+  const clickedParentElem = clickedTodoItem.parentNode;
+  clickedParentElem.removeChild(clickedTodoItem);
 }
