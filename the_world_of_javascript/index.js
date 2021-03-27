44d8567e589995a071f86d42d24a704b021f68f9
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-03-26 20:47:17 -0400

    build(twovjs): adjust code spacing

--- the_world_of_javascript/index.js
+++ the_world_of_javascript/index.js
@@ -1,15 +1,17 @@
 "use strict";
 
 // Access elements.
+
 const addTodoTextElem = document.querySelector(`input#add-todo-text`);
 const addTodoButtonElem = document.querySelector(`button#add-todo-button`);
 const todoListElem = document.querySelector(`ul#todo-list`);
 
 // Add event listeners.
+
 addTodoButtonElem.addEventListener("click", addTodoItem);
 
 // Define event actions.
-function addTodoItem() {
+
   const newTodoText = addTodoTextElem.value.trim();
   if (newTodoText.length === 0) {
     return;
