e596a66279ad68c2a9922a1ef62f48bda13177be
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-03-29 16:27:05 -0400

    feat(ptwov): remake todo list app with Vue

--- previewing_the_world_of_vue/index.js
+++ previewing_the_world_of_vue/index.js
@@ -1,1 +1,51 @@
 "use strict";
+
+// Setup Vue.
+
+new Vue({
+  el: "#app",
+  data: {
+    todoList: [],
+    newTodoText: "",
+  },
+  methods: {
+    addTodo,
+    removeTodo,
+  },
+});
+
+// Define event actions.
+
+function addTodo() {
+  // Get user input.
+  this.newTodoText = this.newTodoText.trim();
+  if (this.newTodoText.length === 0) {
+    return;
+  }
+  // Create new todo item data and add it to todo list data.
+  const newTodo = {
+    id: uniqueId(),
+    text: this.newTodoText,
+  };
+  // console.log(newTodo);
+  this.todoList.push(newTodo);
+  // console.log(this.todoList);
+}
+
+function removeTodo(todo) {
+  // Get todo item.
+  const todoId = todo.id;
+  // Remove todo item from todo list data and element.
+  const todoIndex = this.todoList.findIndex(
+    (item) => item.id === todoId
+  );
+  // console.log(todoIndex);
+  this.todoList.splice(todoIndex, 1);
+  // console.log(this.todoList);
+}
+
+function uniqueId() {
+  const timestamp = Date.now().toString();
+  const randomDecimals = Math.random().toFixed(18).slice(1);
+  return `${timestamp}${randomDecimals}`; /* this should be unique */
+}
