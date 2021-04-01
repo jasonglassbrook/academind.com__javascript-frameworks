54edb7eb48dce8237619622ace3bd97593db19c2
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-04-01 18:46:53 -0400

    feat(twov): add name-changer subapp
    NOTE: Some reformatting of the html came along for the ride.

--- the_world_of_vue/index.js
+++ the_world_of_vue/index.js
@@ -1,1 +1,48 @@
 "use strict";
+
+/***************************************
+  Vue Config
+***************************************/
+
+const initialName = "Jason Glassbrook";
+
+new Vue({
+  el: "#app",
+  data: {
+    initialName,
+    name: initialName,
+  },
+  methods: {
+    changeName,
+  },
+});
+
+/***************************************
+  Event Handlers
+***************************************/
+
+function changeName() {
+  this.name = reverseName(this.name);
+}
+
+function addElement() {}
+
+/***************************************
+  Tools
+***************************************/
+
+function reverseName(s) {
+  return toTitleCase(reverseString(s));
+}
+
+function reverseString(s) {
+  return s.split("").reverse().join("");
+}
+
+function toTitleCase(s) {
+  return s.replace(/\w+/g, toUpperFirstCase);
+}
+
+function toUpperFirstCase(s) {
+  return s.charAt(0).toLocaleUpperCase() + s.slice(1).toLocaleLowerCase();
+}
